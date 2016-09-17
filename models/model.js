ed87a02fd0137e9f0ecc928cdfde4b5a72a3409d
Theo Anderson <theoanderson3@gmail.com>
2016-09-17 11:55:44 -0400

    second commit

--- models/model.js
+++ models/model.js
@@ -3,11 +3,11 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema();
 
 var model = new mongoose.Schema({
-	name: String,
-	dynos: Number,
-	frequency: String,
-	lastRun: Date,
-	scheduled: Date
+	name: { type: String, required: true },
+	dynos: { type: Number, required: true },
+	frequency: { type: String, required: true },
+	lastRun: { type: Date, required: true },
+	scheduled: {type: Date, required: true }
 });
 
 
---
+++ public/app.js
@@ -0,0 +1,51 @@
+(function() {
+    var app = angular.module('app', ['ngRoute']);
+
+    app.controller('HomeController', function($http, $scope) {
+        var vm = this;
+        vm.jobs = [];
+
+        vm.saveJob = function(job) {
+            console.log(job);
+            if (job) {
+                $http.put('/api/jobs', job).then(function(res) {
+                    console.log(res);
+                    vm.getCurrentJobs();
+                })
+            }
+        }
+
+        vm.deleteJob = function(job) {
+            $http.delete('/api/jobs/' + job._id).then(function(response) {
+                console.log('has been deelted');
+                vm.getCurrentJobs();
+            })
+        }
+
+        vm.addJob = function(job) {
+            $http.post('/api/jobs', job).then(function(res) {
+                vm.model = '';
+                console.log(res);
+
+            })
+            vm.getCurrentJobs();
+        }
+
+        vm.getCurrentJobs = function() {
+            $http.get('/api/jobs').then(function(response) {
+                vm.jobs = response.data;
+            })
+        }
+        vm.getCurrentJobs();
+        vm.title = "Angular Scheduler";
+    });
+
+    app.config(function($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'HomeController',
+            controllerAs: 'vm',
+            templateUrl: './home.html'
+        });
+        $routeProvider.otherwise('/');
+    })
+}())
